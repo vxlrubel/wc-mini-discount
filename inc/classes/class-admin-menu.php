743b40e3f3edae00768217b8b58b195ecdfae72f
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-17 17:14:24 +0600

    set category

--- inc/classes/class-admin-menu.php
+++ inc/classes/class-admin-menu.php
@@ -32,9 +32,49 @@ class Admin_Menu{
      * @return void
      */
     private function set_discount_price(){
-        if ( isset( $_REQUEST['set_discount']) && ! empty( $_REQUEST['set_discount'] ) ){
+        if ( isset( $_REQUEST['set_discount'] ) && ! empty( $_REQUEST['set_discount'] ) ){
             update_option( '_wc_mini_discount', $_REQUEST['set_discount'] );
         }
+    }
+
+    /**
+     * add category
+     *
+     * @return void
+     */
+    public function add_category(){
+        global $wpdb;
+        $table = $wpdb->prefix . 'wc_mini_discount';
+
+        if ( isset( $_REQUEST['add_category'] ) && ! empty( $_REQUEST['add_category'] ) ){
+            $category_name = sanitize_text_field( $_REQUEST['add_category'] );
+
+            $data = [
+                'name' => $category_name
+            ];
+
+            $wpdb->insert( $table, $data );
+        }
+    }
+
+    /**
+     * get results
+     *
+     * @return void
+     */
+    private function get_results(){
+        global $wpdb;
+        $table = $wpdb->prefix . 'wc_mini_discount';
+        $sql = "SELECT name FROM $table";
+        $results = $wpdb->get_results( $sql, ARRAY_A );
+
+        $items = [];
+
+        foreach ( $results as $result ) {
+            $items[] = $result['name'];
+        }
+        
+        return $items;
     }
 
     /**
@@ -45,6 +85,8 @@ class Admin_Menu{
     public function render_page_content(){
         $action = $_SERVER['PHP_SELF'] . '?page=wc-mini-discount';
         $this->set_discount_price();
+        $this->add_category();
+        $items = $this->get_results();
         ?>
         <div class="wrap wc-mini-discount">
             <h2>WC Mini Discount</h2>
@@ -71,6 +113,11 @@ class Admin_Menu{
                 </div>
                 <div class="right-side">
                     <ul>
+                        <?php
+                            foreach ( $items as $item ) {
+                                printf( '<li>%s</li>', $item );
+                            }
+                        ?>
                         <li> category items</li>
                     </ul>
                 </div>
