a7758f88a0e231ce09502fda23576d55445b9dce
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-17 15:23:56 +0600

    include admin class for create admin menu

--- inc/autoload.php
+++ inc/autoload.php
@@ -3,3 +3,16 @@
 
 // dierectly access denied
 defined('ABSPATH') || exit;
+
+$files = [
+    'classes/class-admin-menu',
+];
+
+foreach ( $files as $file ) {
+    if( file_exists(  dirname( __FILE__ ) . '/' . $file . '.php' ) ){
+        require_once dirname( __FILE__ ) . '/' . $file . '.php';
+    }else{
+        $not_found = dirname(__FILE__) . '/' . $file . '.php';
+        throw new Exception("file not found {$not_found}", 1);
+    }
+}
