00c690344078e1c68d3d7cb7a8b9d43ba91416a5
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-20 17:43:54 +0600

    insert data into the table

--- inc/classes/class-admin-menu.php
+++ inc/classes/class-admin-menu.php
@@ -65,7 +65,7 @@ class Admin_Menu{
     private function get_results(){
         global $wpdb;
         $table = $wpdb->prefix . 'wc_mini_discount';
-        $sql = "SELECT ID, category_slug, discount_price FROM $table";
+        $sql = "SELECT ID, category_slug, discount_price FROM $table ORDER BY ID DESC";
         $results = $wpdb->get_results( $sql, ARRAY_A );
 
         $items = [];
@@ -78,6 +78,39 @@ class Admin_Menu{
     }
 
     /**
+     * insert category with discount price
+     *
+     * @return void
+     */
+    public function insert_category_discount(){
+        global $wpdb;
+        $table = $wpdb->prefix . 'wc_mini_discount';
+
+        if ( isset( $_REQUEST['category_slug'] ) && ! empty( $_REQUEST['category_slug'] ) && isset( $_REQUEST['discount_price'] ) &&  ! empty( $_REQUEST['discount_price'] ) ) {
+            $category_slug  = sanitize_text_field( $_REQUEST['category_slug'] );
+            $discount_price = trim( $_REQUEST['discount_price'] );
+
+            $data = [
+                'category_slug'  => $category_slug,
+                'discount_price' => $discount_price,
+            ];
+
+            $result = $wpdb->insert( $table, $data );
+
+            if( $result === false ){
+                echo 'something went wrong.';
+            }
+        }else{
+            if ( isset( $_REQUEST['discount_price'] ) && empty( $_REQUEST['discount_price'] ) ){
+                printf(
+                    '<div class="notice notice-error is-dismissible"><p>%s</p></div>',
+                    esc_html( 'Discount price is not set.' )
+                );
+            }
+        }
+    }
+
+    /**
      * render the page content
      *
      * @return void
@@ -86,6 +119,7 @@ class Admin_Menu{
         $action = $_SERVER['PHP_SELF'] . '?page=wc-mini-discount';
         $this->set_discount_price();
         $this->add_category();
+        $this->insert_category_discount();
         $items = $this->get_results();
         $link  = esc_url( admin_url( 'admin.php?page=wc-mini-discount' ) );
 
@@ -97,45 +131,42 @@ class Admin_Menu{
 
             <div class="wrapper-box">
                 <div class="left-side">
-                    <h3 class="inner-title">Current Discount : <?php echo $get_discount_count; ?>% </h3>
-                    <!-- set discount -->
-                    <form action="<?php echo esc_url( $action ); ?>" method="POST">
-                        <label for="set-price">Set Price: </label>
-                        <input type="number" id="set-price" name="set_discount" min="0" max="100" class="regular-text">
+                    <h3 class="inner-title">Add New Discount Offers!</h3>
+
+                    <form action="<?php echo $action; ?>" method="POST">
+                        <div class="form-box">
+                            <div class="category">
+                                <?php
+                                    $get_categories = get_terms(
+                                        [
+                                            'taxonomy'   => 'product_cat',
+                                            'hide_empty' => false,
+                                        ]
+                                    );
+                                    echo '<label for="add-category">Add Category Name: </label>';
+                                    echo '<select class="widefat" name="category_slug" id="add-category">';
+
+                                    foreach ( $get_categories as $category ) {
+                                        printf(
+                                            '<option value="%1$s">%2$s</option>',
+                                            esc_attr( $category->name ),
+                                            esc_html__( $category->name, 'wc-mini-discount' ),
+                                        );
+                                    }
+                                    echo '</select>';
+                                ?>
+                            </div>
+                            <div class="discount-price">
+                                <label for="set-price">Set Price: </label>
+                                <input type="number" id="set-price" name="discount_price" min="0" max="100" class="widefat">
+                            </div>
+                        </div>
                         <p>
-                            <input type="submit" value="Set Discount Price" class="button button-primary">
+                            <input type="submit" value="Add New Discount" class="button button-primary">
                         </p>
                     </form>
-
-                    <!-- add category -->
-                    <form action="<?php echo esc_url( $action ); ?>" method="POST">
-
-
-                        <?php
-                            $get_categories = get_terms(
-                                [
-                                    'taxonomy'   => 'product_cat',
-                                    'hide_empty' => false,
-                                ]
-                            );
-                            echo '<label for="add-category">Add Category Name: </label>';
-                            echo '<select class="regular-text" name="add_category" id="add-category">';
-
-                            foreach ( $get_categories as $category ) {
-                                printf(
-                                    '<option value="%1$s">%2$s</option>',
-                                    esc_attr( $category->name ),
-                                    esc_html__( $category->name, 'wc-mini-discount' ),
-                                );
-                            }
-                            echo '</select>';
-                        ?>
-                        <p>
-                            <input type="submit" value="Add New Category" class="button button-primary">
-                        </p>
-                    </form>
-
                 </div>
+
                 <div class="right-side">
                     <h3 class="inner-title">Category List</h3>
                     <table class="wp-list-table widefat striped">
