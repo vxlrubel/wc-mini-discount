14e9c71e6ce3c09d8536fb5d0c3236e7716472a7
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-23 15:06:13 +0600

    implement edit form

--- inc/classes/class-admin-menu.php
+++ inc/classes/class-admin-menu.php
@@ -136,6 +136,66 @@ class Admin_Menu{
     }
 
     /**
+     * generate edit form
+     *
+     * @return void
+     */
+    private function get_edit_form(){
+        global $wpdb;
+        $table  = $wpdb->prefix . 'wc_mini_discount';
+        $id     = (int) $_REQUEST['discount_edit'];
+        $sql    = "SELECT * FROM $table WHERE ID = $id";
+        $result = $wpdb->get_results( $sql, ARRAY_A );
+        $action = $_SERVER['PHP_SELF'] . '?page=wc-mini-discount';
+
+        
+        foreach ( $result as $row ) : ?>
+            <form action="<?php echo $action; ?>" method="POST">
+                <div class="form-box">
+                    <div class="category">
+                        <?php
+                            $get_category_from_db       = sanitize_text_field( $row['category_slug'] );
+                            $get_discount_prise_from_db = (int) $row['discount_price'];
+
+                            $get_categories = get_terms(
+                                [
+                                    'taxonomy'   => 'product_cat',
+                                    'hide_empty' => false,
+                                ]
+                            );
+                            echo '<label for="add-category">Add Category Name: </label>';
+                            echo '<select class="widefat" name="update_category_slug" id="add-category">';
+
+                            foreach ( $get_categories as $category ) {
+                                printf(
+                                    '<option value="%1$s" %2$s>%3$s</option>',
+                                    esc_attr( $category->name ),
+                                    selected( $category->name, $get_category_from_db, false ),
+                                    esc_html__( $category->name, 'wc-mini-discount' ),
+                                );
+                            }
+                            echo '</select>';
+                        ?>
+                    </div>
+                    <div class="discount-price">
+                        <label for="set-price">Set Price: </label>
+                        <input 
+                            type="number"
+                            id="set-price"
+                            value="<?php echo esc_attr( $get_discount_prise_from_db ); ?>"
+                            name="update_discount_price"
+                            min="0" max="100" class="widefat"
+                        >
+                    </div>
+                </div>
+                <p>
+                    <input type="submit" value="Update" class="button button-primary">
+                </p>
+            </form>
+        <?php endforeach;
+    }
+
+    /**
      * render the page content
      *
      * @return void
@@ -158,38 +218,45 @@ class Admin_Menu{
                 <div class="left-side">
                     <h3 class="inner-title">Add New Discount Offers!</h3>
 
-                    <form action="<?php echo $action; ?>" method="POST">
-                        <div class="form-box">
-                            <div class="category">
-                                <?php
-                                    $get_categories = get_terms(
-                                        [
-                                            'taxonomy'   => 'product_cat',
-                                            'hide_empty' => false,
-                                        ]
-                                    );
-                                    echo '<label for="add-category">Add Category Name: </label>';
-                                    echo '<select class="widefat" name="category_slug" id="add-category">';
+                    <?php 
+                        if ( isset( $_REQUEST['discount_edit'] ) && ! empty( $_REQUEST['discount_edit'] ) ){
+                            $this->get_edit_form();
+                        }else{ ?>
+                            <form action="<?php echo $action; ?>" method="POST">
+                                <div class="form-box">
+                                    <div class="category">
+                                        <?php
+                                            $get_categories = get_terms(
+                                                [
+                                                    'taxonomy'   => 'product_cat',
+                                                    'hide_empty' => false,
+                                                ]
+                                            );
+                                            echo '<label for="add-category">Add Category Name: </label>';
+                                            echo '<select class="widefat" name="category_slug" id="add-category">';
 
-                                    foreach ( $get_categories as $category ) {
-                                        printf(
-                                            '<option value="%1$s">%2$s</option>',
-                                            esc_attr( $category->name ),
-                                            esc_html__( $category->name, 'wc-mini-discount' ),
-                                        );
-                                    }
-                                    echo '</select>';
-                                ?>
-                            </div>
-                            <div class="discount-price">
-                                <label for="set-price">Set Price: </label>
-                                <input type="number" id="set-price" name="discount_price" min="0" max="100" class="widefat">
-                            </div>
-                        </div>
-                        <p>
-                            <input type="submit" value="Add New Discount" class="button button-primary">
-                        </p>
-                    </form>
+                                            foreach ( $get_categories as $category ) {
+                                                printf(
+                                                    '<option value="%1$s">%2$s</option>',
+                                                    esc_attr( $category->name ),
+                                                    esc_html__( $category->name, 'wc-mini-discount' ),
+                                                );
+                                            }
+                                            echo '</select>';
+                                        ?>
+                                    </div>
+                                    <div class="discount-price">
+                                        <label for="set-price">Set Price: </label>
+                                        <input type="number" id="set-price" name="discount_price" min="0" max="100" class="widefat">
+                                    </div>
+                                </div>
+                                <p>
+                                    <input type="submit" value="Add New Discount" class="button button-primary">
+                                </p>
+                            </form>
+                        <?php }
+                    ?>
+                    
                 </div>
 
                 <div class="right-side">
@@ -201,6 +268,7 @@ class Admin_Menu{
                                 <th>Discount Price</th>
                                 <th>Actions</th>
                             </tr>
+
                         </thead>
                         <tbody>
 
